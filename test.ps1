ea72aa0248c9b98a8d0c53997ac058531ee5f38c
maximilian-s-puglielli <maxim.pugli@protonmail.com>
2021-08-07 16:48:28 -0700

    Added: powershell scripts for making, cleaning, & testing

---
+++ test.ps1
@@ -0,0 +1,30 @@
+### Copyright (C) 2021 Maximilian S Puglielli (MSP)
+###
+### The full copyright license belonging to this repository may be found in the
+### parent directory in the file named 'LICENSE'.
+###
+### This program is free software: you can redistribute it and/or modify it
+### under the terms of the GNU General Public License as published by the Free
+### Software Foundation, either version 3 of the License, or (at your option)
+### any later version.
+###
+### This program is distributed in the hope that it will be useful, but WITHOUT
+### ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+### FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+### more details.
+###
+### You should have received a copy of the GNU General Public License along with
+### this program.  If not, see <https://www.gnu.org/licenses/>.
+###
+### AUTHOR:  Maximilian S Puglielli (MSP)
+### CREATED: 2021.08.06
+
+if ( Test-Path -Path "build\Main\Debug\RunAllTests.exe" )
+{
+	& "build\Main\Debug\RunAllTests.exe"
+}
+else
+{
+	& "make.ps1"
+	& "test.ps1"
+}
